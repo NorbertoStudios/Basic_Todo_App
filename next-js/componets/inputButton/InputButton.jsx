5ed53db2cd46fba43dc592c438e520bf722bdc0d
Norberto Fernandez <34346031+NorbertoStudios@users.noreply.github.com>
2022-08-11 01:29:30 -0400

    added inputbox and inputbutton components

---
+++ next-js/componets/inputButton/InputButton.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import styles from '../../styles/input/InputButton.module.css'
+
+const InputButton = ({ label, getInput }) => {
+    return (
+        <button className={styles.btn}
+            onClick={getInput}
+            type="button"
+        >
+            {label}
+        </button>
+    )
+}
+
+export default InputButton
