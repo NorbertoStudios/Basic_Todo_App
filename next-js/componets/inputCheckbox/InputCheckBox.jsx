b1ee93ee3842015fb6c666fc491d355649b01c66
Norberto Fernandez <34346031+NorbertoStudios@users.noreply.github.com>
2022-08-11 04:01:13 -0400

    remove comment

--- next-js/componets/inputCheckbox/InputCheckBox.jsx
+++ next-js/componets/inputCheckbox/InputCheckBox.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 import styles from '../../styles/list/ShowList.module.css'
 
 const InputCheckBox = ({ selected, handle }) => {
-    // const [c, setC] = React.useState(false);
-    // setC(check);
     return (
         <input
             className={styles.listCheckbox}
