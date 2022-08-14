7eff8d2c387564284b5cc92b3695e4ba7b98232b
Norberto Fernandez <34346031+NorbertoStudios@users.noreply.github.com>
2022-08-14 13:34:09 -0400

    testing working directory 06

--- next-js/next.config.js
+++ next-js/next.config.js
@@ -1,16 +1,11 @@
 /** @type {import('next').NextConfig} */
 
-const githubConfig = {
-  assetPrefix: "/Basic_Todo_App/",
-  basePath: "/Basic_Todo_App",
-};
-
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  githubConfig,
-
-}
+  assetPrefix: "/Basic_Todo_App/",
+  basePath: "/Basic_Todo_App",
+};
 
 
 module.exports = nextConfig;
