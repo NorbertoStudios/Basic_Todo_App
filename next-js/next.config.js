59dc516df9c1205e08376c907b0c8f2173cc67c2
Norberto Fernandez <34346031+NorbertoStudios@users.noreply.github.com>
2022-08-14 12:32:47 -0400

    fix babel warning, fix for github pages

--- next-js/next.config.js
+++ next-js/next.config.js
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
+
+const githubConfig = {
+  assetPrefix: "/Basic_Todo_App/",
+  basePath: "/Basic_Todo_App",
+};
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  githubConfig,
+
 }
 
-module.exports = nextConfig
+
+module.exports = nextConfig;
