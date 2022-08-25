7f22fd5556b140d8de50d0a535e39432a78aab5c
NorbertoStudios <norbertostudios@gmail.com>
2022-08-25 00:55:47 -0400

    clean up

--- next-js/next.config.js
+++ next-js/next.config.js
@@ -12,20 +12,20 @@ const nextConfig = {
   // basePath: "/Basic_Todo_App",
   reactStrictMode: true,
   swcMinify: true,
-  images: {
-    //   domains: [
-    //     "localhost/Basic_Todo_App",
-    //     "https://norbertostudios.github.io/Basic_Todo_App",
-    //   ],
-    loader: "imgix",
-    // path: "https://norbertostudios.github.i/Basic_Todo_App"
-    path: "https://basic-todo.norbertostudios.com",
-  },
-  experimental: {
-    images: {
-      unoptimized: true,
-    },
-  },
+  // images: {
+  //   //   domains: [
+  //   //     "localhost/Basic_Todo_App",
+  //   //     "https://norbertostudios.github.io/Basic_Todo_App",
+  //   //   ],
+  //   loader: "imgix",
+  //   // path: "https://norbertostudios.github.i/Basic_Todo_App"
+  //   path: "https://basic-todo.norbertostudios.com",
+  // },
+  // experimental: {
+  //   images: {
+  //     unoptimized: true,
+  //   },
+  // },
 };
 
 // Dev Local Host
